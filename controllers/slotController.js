d11e7863df61977e2f856e1e5bb8a5b2779de013
nsilveri <nicola.silveri@hotmail.it>
2022-07-14 16:20:22 +0200

    prova funzione diffhour copilot

--- controllers/slotController.js
+++ controllers/slotController.js
@@ -10,6 +10,12 @@ class slotController {
         // si scalano i soldi al consumer
         // si aggiorna il db transazioni con l'esito della transazione attuale
 
+        //funzione per calcolare la differenza di almeno 24 ore tra 2 date
+        function diffHours(date1, date2) {
+            var diff = (date2.getTime() - date1.getTime()) / 1000;
+            diff /= 60 * 60;
+            return Math.abs(Math.round(diff));
+        }
         
 
     }
