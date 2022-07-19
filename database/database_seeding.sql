9caf989729333bfe298fd39d240b02cf6675672b
Simo-univpm <s1102917@studenti.univpm.it>
2022-07-19 14:20:13 +0200

    seeding

--- database/database_seeding.sql
+++ database/database_seeding.sql
@@ -81,3 +81,30 @@ CREATE TABLE db_transazioni(
   data_acquisto_transazione DATETIME NOT NULL,
   data_prenotazione_transazione DATETIME NOT NULL,
 );
+
+
+INSERT INTO prodotto (nome_prodotto, tipologia, anno, prezzo, disponibile, link)
+VALUES 
+    ('U2 - One', 'Audio', 1992, 2, false, null),
+    ('U2 - One (Official Music Video)', 'Video', 1992, 3, false, null),
+    ('Mahmood, BLANCO - Brividi', 'Audio', 2022, 2, true, './files/Mahmood, BLANCO - Brividi.mp3'),
+    ('Pinguini Tattici Nucleari - Irene', 'Audio', 2018, 2, true, './files/Pinguini Tattici Nucleari - Irene.mp3'),
+    ('Radiohead - Creep (Official Music Video)', 'Video', 1992, 3, true, './files/Radiohead - Creep (Official Music Video).mp4');
+
+INSERT INTO utente (username, passwd, cognome, nome, mail, ruolo, indirizzo, credito)
+VALUES 
+    ('paolo95','$2a$12$zHLBzgjqH1xIDLI0Oasr6..4i0VYWlfWOLc1sIKl8tYPFXs1e.1s2', 'Compagnoni', 'Paolo', 'compagnonipaolo95@gmail.com', 'admin', 'Viale Piane San Donato 33 - Corropoli (TE)', 7),
+    ('simone95','$2a$12$huCmEyw5m2thUh.2P6vEmeG/SPbGEN5TTOlEteDsbTkBbL0sLUvam', 'Onori', 'Simone', 'simoenonori@gmail.com', 'user', 'Via Napoli 2/A - Ascoli Piceno (AP)', 7),
+    ('faini98','$2a$12$Y8DXqFtpNeTzlBnm8cHZqupe1eG/EBOAySqkGs1REDhedtQmGjmKa', 'Faini', 'Aurora', 'aurorafaini@gmail.com', 'user', 'Via Giuseppe Mazzini 22 - Monteprandone (AP)', 7),
+    ('schiavi92','$2a$12$rccf3ddKBAtF00Uzz38xA.8q5yJ/swYqHWS9jT3Ze1U0gfhYemCS.', 'Schiavi', 'Elise', 'eliseschiavi@gmail.com', 'admin', 'Via dell Olmo 7 - San Benedetto del Tronto (AP)', 7);
+
+INSERT INTO acquisto (utente, prodotto, data_acquisto, originale, mail_amico, download_amico)
+VALUES 
+    (1, 2, '2022-03-02', true, null, null),
+    (1, 2, '2002-03-02', false, null, null),
+    (1, 3, '2022-07-21', false, null, null),
+    (2, 2, '2022-11-20', false, null, null),
+    (1, 3, '2022-01-21', true, 'prova@prova.com', false),
+    (2, 2, '2022-04-20', true, 'tizio@tizio.com', true),
+    (2, 3, '2022-07-12', true, null, null),
+    (3, 3, '2021-08-06', true, null, null);
